95434471d83b57a53c49db01894e68630c6a7fe5
Matheus Martins <matheusmmartins8@gmail.com>
2022-03-24 12:54:23 -0300

    Correções finais: primeira parte do processo de cadastro de usuários funcionando corretamente.

--- src/models/RegisterModel.js
+++ src/models/RegisterModel.js
@@ -34,13 +34,13 @@ class Register {
         if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido.');
 
         // password válido
-        if(this.body.password < 3 || this.body.password > 50) {
-            this.errors.push('Senha inválida.');
+        if(this.body.password.length < 3 || this.body.password.length > 50) {
+            this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
         }
     }
 
     clearUp() {
-        for(key in this.body) {
+        for(let key in this.body) {
             if(typeof this.body[key] !== 'string') {
                 this.body[key] == '';
             }
